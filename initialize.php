19547c6f71731c18897572cc318a2d5384a222ad
Arzel John Zolina <Arzeljrz17@gmail.com>
2024-05-20 19:33:29 +0800

    meow

--- initialize.php
+++ initialize.php
@@ -5,5 +5,5 @@ if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
 if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "arzelzolina10");
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "");
 if (!defined('DB_NAME')) define('DB_NAME', "store");
